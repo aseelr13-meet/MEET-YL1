57ba14c7b48379229c6a7446ce99cff453049ad2
Guest <guest-PmXhU5@meet.mit.edu>
2013-11-01 14:56:18 +0200

    palindrome.py

--- lab2/palindrome.py
+++ lab2/palindrome.py
@@ -1,8 +1,12 @@
 def ispalindrome(n):
 	n = str()
+theword = range(n)
+for 
+	 for theword in ispalindrome(n)
+		print true
+	for 
 
-
+ 
 if __name__=='__main__':
 	n = raw_input("write a palindrome: ")
-	len(n)
 	ispalindrome(n)
