0a9a6375c092174ca13adf5650127344e3408611
Guest <guest-PmXhU5@meet.mit.edu>
2013-11-01 14:49:13 +0200

    divisors.py

--- lab2/divisors.py
+++ lab2/divisors.py
@@ -1,6 +1,10 @@
-def divisors():
-	n = int(raw_input("enter a number"))
+def divisors(n):
+	n = int(n)
 	for num in range(1,n+1):
 		if n % num == 0:
 			print num
-divisors()
+
+if __name__=='__main__':
+	n = raw_input("enter a number ")
+	divisors(n)
+
---
+++ lab2/palindrome.py
@@ -0,0 +1,8 @@
+def ispalindrome(n):
+	n = str()
+
+
+if __name__=='__main__':
+	n = raw_input("write a palindrome: ")
+	len(n)
+	ispalindrome(n)
